74c2be7501c306b0cea2259889bcf35caa7eff6b
WeiGangqiang <284894567@qq.com>
2018-06-22 11:26:17 +0800

    fix bug

--- README.md
+++ README.md
@@ -18,7 +18,30 @@
 ]
 ```
 
-##2.0 查询intent列表
+## 2.0查询agent未识别的话
+* 请求方式：
+
+```get http://localhost:port/unknown-says```
+
+* 参数
+
+| Param | Type | Description |
+| --- | --- | --- |
+| agent | `String` | agent的名字（course-record） |
+
+* 响应
+```
+[
+    "的科一刻不知道",
+    "暂停播放春秋",
+    "停机他听身体提特带天的体力和兔子",
+    "我们家谁的神经病都读课文的时候要快一点小爱同学",
+    "我们俩谁是神经病",
+    "打开如意古诗词"
+]
+```
+
+## 3.0 查询intent列表
 * 请求方式：
 
 ```get http://localhost:port/intents```
@@ -48,7 +71,7 @@
 ]
 ```
 
-##3.0 查询intent parameter列表
+## 4.0 查询intent parameter列表
 * 请求方式
 
 ``` get http://localhost:port/parameters```
--- app.js
+++ app.js
@@ -29,6 +29,26 @@ app.get("/parameters", async function(req, res){
     res.send(paras)
 })
 
+app.post("/corpus", async function(req, res){
+    const msg = req.body
+    const agent = msg.agent
+    const intentId = msg.intentId
+    const user_say = msg.user_say
+
+    res.send({result: "success"})
+})
+
+app.get("/unknown-says", async function(req, res){
+    var unknowns = ["的科一刻不知道",
+                    "暂停播放春秋",
+                    "停机他听身体提特带天的体力和兔子",
+                    "我们家谁的神经病都读课文的时候要快一点小爱同学",
+                    "我们俩谁是神经病",
+                    "打开如意古诗词"]
+
+    res.send(unknowns)
+})
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
