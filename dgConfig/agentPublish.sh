1ca297e31096487ac31f9db0ee08a2fc8c433836
WeiGangqiang <284894567@qq.com>
2018-09-09 12:16:49 +0800

    fix bug

--- dgConfig/agentPublish.sh
+++ dgConfig/agentPublish.sh
@@ -1,2 +1,17 @@
 #！/bin/bash 
-echo "Hello word!" 
+agentName="corpus-test"
+echo "begin publish for $agentName ..." 
+cd ../
+
+echo "begin dg for $agentName ...." 
+cd ./chatbot-dg
+sbt "run corpus-test"
+cd ../
+echo "dg finish for $agentName"
+
+
+echo "begin fastText train for $agentName..."
+cd ./chatbot-config/script
+python3 fastTextTrain.py users/$agentName
+cd ../../
+echo "end fastText train for $agentName"
